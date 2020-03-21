2d3d4cd76a3da4ef89f43c6b070de4264b9bd984
Erik Prenzlin <eprenzlin@gmail.com>
2020-03-21 16:13:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,34 @@
 # Add your code here
+require "pry"
+class Dog
+
+  @@all =[]
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.print_all
+    @@all.each do |a|
+      puts a.name
+    end
+  end
+
+
+# PRIVATE
+  def save_dog
+    @@all << self
+  end
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    
+  end
+end
